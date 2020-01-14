389b2f1f1c7a341cbed5320a08f3707357959816
Zhong Wan <zhowa@microsoft.com>
2020-01-14 18:38:12 +0800

    remove unused codes

--- decision_tree_cancer.py
+++ decision_tree_cancer.py
@@ -1,22 +1,9 @@
-import graphviz
-
 from sklearn.tree import (
     DecisionTreeClassifier,
     export_graphviz,
     )
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
-
-
-def build_tree(max_depth=None):
-    tree = DecisionTreeClassifier(max_depth=max_depth)
-    tree.fit(X_train, y_train)
-    print("Max Depth: {0}".format(max_depth))
-    print("Training Accuracy: {0:.2f}".format(tree.score(X_train, y_train)))
-    print("Testing Accuracy: {0:.2f}".format(tree.score(X_test, y_test)))
-    print()
-    return
-
 
 if __name__ == "__main__":
     cancer = load_breast_cancer()
